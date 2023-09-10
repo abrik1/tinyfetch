e4aef3bd133a237d27a88ce15b29572613682f35
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-09-10 18:35:44 +0530

    thanks to @jtbx for smaller code

--- tinyfetch.c
+++ tinyfetch.c
@@ -2,14 +2,10 @@
 #include <stdlib.h>
 
 int main(void){
-    printf("\x1b[31muser:\x1b[0m ");
-    printf(getenv("USER"));
-    printf("\n\x1b[32msh: \x1b[0m");
-    printf(getenv("SHELL"));
-    printf("\n\x1b[33mterm:\x1b[0m ");
-    printf(getenv("TERM"));
-    printf("\n\x1b[34mlocale:\x1b[0m ");
-    printf(getenv("LANG"));
-    printf("\n\x1b[35mcolors: \x1b[31m▅▅\x1b[32m▅▅\x1b[33m▅▅\x1b[34m▅▅\x1b[35m▅▅\x1b[0m\n");
-    return 0;
+   printf("\x1b[31muser:\x1b[0m %s\n", getenv("USER"));
+   printf("\x1b[32msh:\x1b[0m %s\n", getenv("SHELL"));
+   printf("\x1b[33mterm:\x1b[0m %s\n", getenv("TERM"));
+   printf("\x1b[34mlocale:\x1b[0m %s\n", getenv("LANG"));
+   printf("\x1b[35mcolors: \x1b[0m\x1b[41m  \x1b[42m  \x1b[43m  \x1b[44m  \x1b[45m  \x1b[0m\n");
+   return 0;
 }
