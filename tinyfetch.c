fed0e2eba704411c0a9ca96379c8f38114753ac5
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-09-12 18:30:14 +0530

    update: now show kewnel and beautiful

--- tinyfetch.c
+++ tinyfetch.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <linux/version.h>
 
 int main(void){
-   printf("\x1b[31muser:\x1b[0m %s\n", getenv("USER"));
-   printf("\x1b[32msh:\x1b[0m %s\n", getenv("SHELL"));
-   printf("\x1b[33mterm:\x1b[0m %s\n", getenv("TERM"));
-   printf("\x1b[34mlocale:\x1b[0m %s\n", getenv("LANG"));
-   printf("\x1b[35mcolors: \x1b[0m\x1b[41m  \x1b[42m  \x1b[43m  \x1b[44m  \x1b[45m  \x1b[0m\n");
+   printf("┌────────┐\n");
+   printf("│\x1b[31m user   \x1b[0m│ %s\n", getenv("USER"));
+   printf("│\x1b[32m sh     \x1b[0m│ %s\n", getenv("SHELL"));
+   printf("│\x1b[33m term   \x1b[0m│ %s\n", getenv("TERM"));
+   printf("│\x1b[34m locale \x1b[0m│ %s\n", getenv("LANG"));
+   printf("│\x1b[35m kernel \x1b[0m│ %d.%d.%d\n", LINUX_VERSION_MAJOR, LINUX_VERSION_PATCHLEVEL, LINUX_VERSION_SUBLEVEL);
+   printf("├────────┤\n");
+   printf("│\x1b[36m colors \x1b[0m│ \x1b[41m  \x1b[42m  \x1b[43m  \x1b[44m  \x1b[45m  \x1b[46m  \x1b[0m\n");
+   printf("└────────┘\n");
    return 0;
 }
